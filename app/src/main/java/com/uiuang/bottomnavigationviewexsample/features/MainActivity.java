eca6603ee6252ac57343e72de8aebfd531ba728e
zsc <zsc7454@163.com>
2021-12-25 13:22:06 +0800

    Initial commit

---
+++ app/src/main/java/com/uiuang/bottomnavigationviewexsample/features/MainActivity.java
@@ -0,0 +1,60 @@
+package com.uiuang.bottomnavigationviewexsample.features;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.uiuang.bottomnavigationviewexsample.R;
+import com.uiuang.bottomnavigationviewexsample.databinding.ActivityMainBinding;
+import com.uiuang.bottomnavigationviewexsample.features.badgeview.BadgeViewActivity;
+import com.uiuang.bottomnavigationviewexsample.features.centerfab.CenterFabActivity;
+import com.uiuang.bottomnavigationviewexsample.features.setupwithviewpager.SetupWithViewPagerActivity;
+import com.uiuang.bottomnavigationviewexsample.features.style.StyleActivity;
+import com.uiuang.bottomnavigationviewexsample.features.viewpager.WithViewPagerActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private ActivityMainBinding bind;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+        bind = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        init();
+    }
+
+    private void init() {
+        bind.btnStyle.setOnClickListener(this);
+        bind.btnWithViewPager.setOnClickListener(this);
+        bind.btnSetupWithViewPager.setOnClickListener(this);
+        bind.btnBadgeView.setOnClickListener(this);
+        bind.btnCenterFab.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_style:
+                startActivity(new Intent(this, StyleActivity.class));
+                break;
+            case R.id.btn_with_view_pager:
+                startActivity(new Intent(this, WithViewPagerActivity.class));
+                break;
+            case R.id.btn_setup_with_view_pager:
+                startActivity(new Intent(this, SetupWithViewPagerActivity.class));
+                break;
+            case R.id.btn_badge_view:
+                startActivity(new Intent(this, BadgeViewActivity.class));
+                break;
+            case R.id.btn_center_fab:
+                startActivity(new Intent(this, CenterFabActivity.class));
+                break;
+
+        }
+    }
+}
